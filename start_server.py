7d135c4254337ac48ba5e3a4a94ac14bc1b77adf
Fahrul Ramadhan Putra <fahrulofficial@gmail.com>
2020-12-02 20:42:40 +0800

    Changing port to work on heroku

--- start_server.py
+++ start_server.py
@@ -2,11 +2,18 @@
 
 import sockets
 import sys
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
 
+if ON_HEROKU:
+    # get the heroku port
+    thePort = int(os.environ.get('PORT', 8080))  # Default port 8080
+else:
+    thePort = 8080
 
 if __name__ == '__main__':  # file is executed
     if len(sys.argv) < 2:
-        sys.argv.append(str(8080))  # Port 8080 default port
+        sys.argv.append(str(thePort))  # Port 8080 default port
     if len(sys.argv) < 3:
         sys.argv.append(str(20))  # 20 moves default maximal return length of maneuver
     if len(sys.argv) < 4:
